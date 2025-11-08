75628cec159de955295be5efbbf0f29e852815a9
aniveshparida <aniveshparida8@gmail.com>
2025-11-08 21:55:49 +0530

    desgined the nav-bar and drop down

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {
+    HEATMAP_WIDGET_CONFIG,
+    MARKET_DATA_WIDGET_CONFIG,
+    MARKET_OVERVIEW_WIDGET_CONFIG,
+    TOP_STORIES_WIDGET_CONFIG
+} from "@/lib/constants";
+import {sendDailyNewsSummary} from "@/lib/inngest/functions";
+
+
+const Home = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+
+        </div>
+    )
+}
+export default Home
